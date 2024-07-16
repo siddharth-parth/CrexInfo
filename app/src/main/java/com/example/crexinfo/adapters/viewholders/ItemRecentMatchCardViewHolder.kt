03023c3611d98b88bb3067f610722aa4d83c46bf
Siddharth Dubey <siddharth.dubey@parth.com>
2024-07-16 15:15:00 +0530

    complete recent matches card

--- app/src/main/java/com/example/crexinfo/adapters/viewholders/ItemRecentMatchCardViewHolder.kt
+++ app/src/main/java/com/example/crexinfo/adapters/viewholders/ItemRecentMatchCardViewHolder.kt
@@ -1,7 +1,10 @@
 package com.example.crexinfo.adapters.viewholders
 
+import android.content.res.ColorStateList
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.crexinfo.R
 import com.example.crexinfo.databinding.ItemRecentMatchCardBinding
 import com.example.crexinfo.helper.FormatHelper.getTeamLogo
 import com.example.crexinfo.helper.ViewHelper
@@ -12,25 +15,47 @@ class ItemRecentMatchCardViewHolder(private val binding: ItemRecentMatchCardBind
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(viewData: BaseViewType) {
-        //TODO:
+        val context = binding.root.context
         val data = viewData as? RecentMatchInfoViewData ?: return
 
         binding.apply {
-            Glide.with(ivTeamOne.context)
+            Glide.with(context)
                 .load(data.teamOneKey.getTeamLogo())
                 .placeholder(ViewHelper.getShimmer())
                 .into(ivTeamOne)
 
-            Glide.with(ivTeamTwo.context)
+            Glide.with(context)
                 .load(data.teamTwoKey.getTeamLogo())
                 .placeholder(ViewHelper.getShimmer())
                 .into(ivTeamTwo)
+
+            tvTeamOne.text = data.teamOneName
+            tvTeamTwo.text = data.teamTwoName
 
             tvTeamOneScore.text = data.teamOneScore
             tvTeamOneOversPlayed.text = data.teamOneOvers
 
             tvTeamTwoScore.text = data.teamTwoScore
             tvTeamTwoOversPlayed.text = data.teamTwoOvers
+
+            tvMatchNumber.text = data.matchNumber
+
+            layoutResultBadge.tvResultBadge.text = viewData.resultString
+            if (viewData.resultString == "W") {
+                layoutResultBadge.tvResultBadge.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.ce_highlight_ac6
+                    )
+                )
+            } else {
+                layoutResultBadge.tvResultBadge.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.ce_highlight_ac4
+                    )
+                )
+            }
         }
     }
 }
